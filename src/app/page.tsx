8cd1ea2d80018e639438cea89403ce6870159cb9
noornazar <noor.nazar.programmer@gmail.com>
2025-02-17 22:22:29 +0500

    style-home page  add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import { ModeToggle } from "@/components/ModeToggle";
-import Image from "next/image";
+import { ModeToggle } from '@/components/ModeToggle';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
-      <ModeToggle />
+    <div className="grid h-screen place-items-center p-8">
+      <main className="flex flex-col items-center justify-center text-center">
+        <h1 className="text-5xl sm:text-7xl font-semibold text-gray-900 dark:text-white">
+          eCommerce Builder
+          <ModeToggle />
+        </h1>
+      </main>
     </div>
   );
 }
